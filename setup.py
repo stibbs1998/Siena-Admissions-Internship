b3e910ba803e0da56fb389a88d1894d5f1068768
stibbs1998 <stibbs1998@gmail.com>
2019-05-28 14:42:16 -0400

    First Commit on admissions_internship.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a project with the al of finding a "critical path" for admitting students to Siena College.',
+    author='Spencer Tibbitts',
+    license='',
+)
